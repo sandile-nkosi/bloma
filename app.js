af64b193b8a7f8502995d9152b5f7b57aba05b63
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2024-07-17 19:04:53 +0200

    main project structure

---
+++ app.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const db = require('./config/database').MongoURI;
+const flash = require('connect-flash');
+const passport = require('passport');
+const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
+const createSessionConfig = require('./config/session');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(createSessionConfig());
+
+
+// Passport session middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_message = req.flash('success_message');
+    res.locals.error_message = req.flash('error_message');
+    next();
+});
+
+
+// Routes
+app.use('/', require('./routes/base.routes'));
+app.use('/student', require('./routes/student.routes'));
+
+mongoose.connect(db).then(()=>{
+    console.log('Database connection established');
+}).catch((err)=>{
+    console.error('Database connection error', err);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`);
+});
